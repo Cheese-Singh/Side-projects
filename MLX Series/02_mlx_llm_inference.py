e1db8b839f038582cd067806294bf479e73f6ea3
Cheese-Singh <ekamveer.singh06@gmail.com>
2026-04-05 21:45:19 +0530

    Update 02_mlx_llm_inference.py

--- MLX Series/02_mlx_llm_inference.py
+++ MLX Series/02_mlx_llm_inference.py
@@ -2,21 +2,33 @@ from mlx_vlm import load, generate
 
 model, processor = load("mlx-community/Qwen3.5-9B-MLX-4bit")
 
+def clean_output(text):
+    stop_tokens = ["<|im_end|>", "<|endoftext|>", "<|im_start|>", "<|im_start|>user"]
+    for token in stop_tokens:
+        if token in text:
+            text = text.split(token)[0].strip()
+    return text
+
 while True:
     prompt = input("User prompt: ")
 
-    if prompt.lower() in ["exit", "quit"]: 
-        break
+    if prompt.lower() in ["exit", "quit"]: break
 
-    text = generate(
+    print("Qwen is thinking...", end="\r", flush=True)
+
+    response = generate(
         model,
         processor,
         prompt=prompt,
-        verbose=True,
+        verbose=False,
         max_tokens=512,
         temperature=0.7,
-        repetition_penalty = 1.1)
-        
+        repetition_penalty = 1.1
+    )
+    
+    reply = clean_output(response.text)
+    print(f"\nQwen: {reply}\n")
+
 inspect = input("Inspect model? (yes/no): ")
 if inspect.lower() == "yes":
     print(model, "\n")
